b38d1eeec9878ba205ba2266a15b53f9a23f9fb2
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-14 18:54:59 +0200

    feat: implement new component structure with navigation and document templates

--- components/Example_templ.go
+++ components/Example_templ.go
@@ -8,7 +8,7 @@ package components
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func Document(content string) templ.Component {
+func Example() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -29,20 +29,7 @@ func Document(content string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Document</title><meta content=\"dark light\" name=\"color-scheme\"></head><body>")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		var templ_7745c5c3_Var2 string
-		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(content)
-		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/document.templ`, Line: 13, Col: 12}
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<p>Example component</p>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- main.go
+++ main.go
@@ -1,45 +1,9 @@
 package main
 
-/**
- * This program looks for folders in the src and files in the components directory and compiles them into a static html files.
- *
- * It expects the following structure:
- *
- * src/
- * ├── some_folder/
- * │   ├── content.html
- * │   └── config.txt
- * └── another_folder/
- *     ├── content.html
- *     └── config.txt
- *
- * components/
- * ├── start.html
- * ├── some_component.html
- * └── another_component.html
- *
- * The config.txt file in each folder should contain the names of the components to include in the html file.
- *
- * The output will be written to the static directory, with each folder's content compiled into a separate html file.
- * The output structure will look like this:
- * static/
- * ├── some_folder.html
- * └── another_folder.html
- *
- * The start.html file will be included at the beginning of each html file, and the end of the html document will be added automatically.
- * The content.html file will be included at the end of each html file.
- * The components will be included in the order they are listed in the config.txt file.
- *
- * The program will log debug messages to the console, which can be useful for troubleshooting.
- * You can change the log level by modifying the log_level variable.
- */
-
 import (
 	"context"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"grapefrui.xyz/vc13/components"
 )
@@ -54,168 +18,32 @@ const (
 )
 
 func main() {
-
-	// Return, if there is no src directory
-	if _, err := os.Stat(src_dir); os.IsNotExist(err) {
-		return
-	}
-
-	// Return, if there is no components directory
-	if _, err := os.Stat(components_dir); os.IsNotExist(err) {
-		log.Println("Components directory does not exist:", components_dir)
-		return
-	}
-
-	// Return, if there is no content.html file in the components directory
-	files, err := os.ReadDir(src_dir)
+	index, err := os.Create("static/index.html")
 	if err != nil {
-		log.Println("Error reading source directory:", err)
-		return
-	}
-	if len(files) == 0 {
-		log.Println("No files found in source directory:", src_dir)
-		return
+		log.Fatalf("failed to create output file: %v", err)
 	}
 
-	// Create the output directory if it does not exist
-	if err := os.MkdirAll(output_dir, os.ModePerm); err != nil {
-		log.Println("Error creating static directory:", err)
-		return
+	example := components.Example()
+
+	with_navigation := components.WithNavigation(example)
+
+	err = components.Document(with_navigation).Render(context.Background(), index)
+	if err != nil {
+		log.Fatalf("failed to write output file: %v", err)
 	}
-	// We collect components that have custom css files. (an array of filenames). We need a Set to avoid duplicates.
-	css_files := make(map[string]bool)
 
-	// Loop over each folder in the source directory
-	for _, file := range files {
-		htmlCache := make([]string, 0) // This will hold the content for the current folder
+	index.Close()
 
-		// Skip if the file is not a directory
-		if file.IsDir() == false {
-			log.Println("Skipping non-directory file in source:", file.Name())
-			continue
-		}
-
-		// Skip if the content.html file does not exist in the directory
-		contentPath := filepath.Join(src_dir, file.Name(), content_filename)
-		if _, err := os.Stat(contentPath); os.IsNotExist(err) {
-			log.Println("Content file does not exist in directory:", contentPath)
-			continue
-		}
-
-		// Skip if the config.txt file does not exist in the directory
-		configPath := filepath.Join(src_dir, file.Name(), config_filename)
-		if _, err := os.Stat(configPath); os.IsNotExist(err) {
-			log.Println("Config file does not exist in directory:", configPath)
-			continue
-		}
-
-		// Read the config.txt file and skip if it is empty
-		configContent, err := os.ReadFile(configPath)
-		if err != nil {
-			log.Println("Error reading config file:", configPath, err)
-			continue
-		}
-
-		// Extract all filenames (words) from the config file
-		component_names := make([]string, 0)
-		lines := strings.Split(string(configContent), "\n")
-		for _, line := range lines {
-			line = strings.TrimSpace(line) // Remove leading/trailing whitespace
-			if line != "" {                // Skip empty lines
-				component_names = append(component_names, line)
-			}
-		}
-
-		// Skip if there are no valid component names in the config file
-		if len(component_names) == 0 {
-			log.Println("No valid component names found in config file:", configPath)
-			continue
-		}
-
-		// Loop over the component file names and add the content to the cache
-		for _, component_name := range component_names {
-			log.Println("Processing component:", component_name)
-
-			// Skip if the component file does not exist
-			componentFile := filepath.Join(components_dir, component_name+".html")
-			if _, err := os.Stat(componentFile); os.IsNotExist(err) {
-				log.Println("Component file does not exist for word:", component_name, "at", componentFile)
-				continue
-			}
-
-			// Skip if there is an error reading the component file
-			componentContent, err := os.ReadFile(componentFile)
-			if err != nil {
-				log.Println("Error reading component file:", componentFile, err)
-				continue
-			}
-
-			// Add the html content to the html cache
-			htmlCache = append(htmlCache, string(componentContent))
-
-			// Check if there is a css file for the component
-			cssFile := filepath.Join(components_dir, component_name+".css")
-			if _, err := os.Stat(cssFile); err == nil {
-				// If the css file exists, add it to the css_files set
-				css_files[component_name] = true
-			}
-		}
-
-		// Read the content of the content.html file and skip if there is an error
-		contentFile := filepath.Join(src_dir, file.Name(), content_filename)
-		contentData, err := os.ReadFile(contentFile)
-		if err != nil {
-			log.Println("Error reading content file:", contentFile, err)
-			continue
-		}
-
-		// Add the content file to the cache
-		htmlCache = append(htmlCache, string(contentData))
-
-		// Write the cache to the content file. E.g. static/about.html
-
-		outputFile := filepath.Join(output_dir, file.Name()+".html")
-		file, err := os.Create(outputFile)
-		if err != nil {
-			log.Println("Error creating output file:", outputFile, err)
-			return
-		}
-		defer file.Close()
-		err = components.Document(strings.Join(htmlCache, "")).Render(context.Background(), file)
-		if err != nil {
-			log.Println("Error rendering document to file:", outputFile, err)
-			return
-		}
-
-		log.Println("Successfully processed directory:", file.Name(), "Output written to:", outputFile)
-
-		// We finally collect all the css files that were used in the components
-		if len(css_files) == 0 {
-			log.Println("No CSS files found for components in directory:", file.Name())
-		}
-		cssCache := make([]string, 0)
-		for cssFile := range css_files {
-			cssPath := filepath.Join(components_dir, cssFile+".css")
-			if _, err := os.Stat(cssPath); os.IsNotExist(err) {
-				log.Println("CSS file does not exist for component:", cssFile, "at", cssPath)
-				continue
-			}
-			cssContent, err := os.ReadFile(cssPath)
-			if err != nil {
-				log.Println("Error reading CSS file:", cssPath, err)
-				continue
-			}
-			cssCache = append(cssCache, string(cssContent))
-		}
-
-		// Write the css files to a single file in the static directory
-		cssOutputFile := filepath.Join(output_dir, css_filename)
-		if err := os.WriteFile(cssOutputFile, []byte(strings.Join(cssCache, "\n")), 0644); err != nil {
-			log.Println("Error writing to CSS output file:", cssOutputFile, err)
-			return
-		}
-		log.Println("Successfully wrote CSS files to:", cssOutputFile)
-
+	about, err := os.Create("static/about.html")
+	if err != nil {
+		log.Fatalf("failed to create about file: %v", err)
 	}
+
+	err = components.Document(with_navigation).Render(context.Background(), about)
+	if err != nil {
+		log.Fatalf("failed to write output file: %v", err)
+	}
+
+	about.Close()
 
 }
