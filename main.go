4c4556ccdf6aab24c29abbd53e52c493194dcca6
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-15 17:10:10 +0200

    refactor: consolidate static file serving and document generation into main function

--- main.go
+++ main.go
@@ -1,76 +1,17 @@
 package main
 
 import (
-	"context"
 	"log"
-	"os"
-
-	"github.com/a-h/templ"
-	"grapefrui.xyz/vc13/layouts"
-	"grapefrui.xyz/vc13/views"
+	"net/http"
 )
 
-func save_document(filename string, content templ.Component) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		log.Fatalf("failed to create output file: %v", err)
-		return err
-	}
-
-	err = layouts.Document(content).Render(context.Background(), file)
-	if err != nil {
-		log.Fatalf("failed to write output file: %v", err)
-		return err
-	}
-
-	file.Close()
-	return nil
-}
-
 func main() {
-	const output_dir = "static"
+	// Serve static files from the "static" folder
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
 
-	welcome := views.Welcome()
-	welcome_with_navigation := layouts.WithNavigation(welcome)
-
-	if err := save_document(output_dir+"/index.html", welcome_with_navigation); err != nil {
-		log.Fatalf("failed to save document: %v", err)
+	log.Println("Server listening on http://localhost:8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
 	}
-
-	drafts := views.Drafts()
-	drafts_with_navigation := layouts.WithNavigation(drafts)
-	if err := save_document(output_dir+"/drafts.html", drafts_with_navigation); err != nil {
-		log.Fatalf("failed to save document: %v", err)
-	}
-
-	signals := views.Signals()
-	signals_with_navigation := layouts.WithNavigation(signals)
-	if err := save_document(output_dir+"/signals.html", signals_with_navigation); err != nil {
-		log.Fatalf("failed to save document: %v", err)
-	}
-
-	intel := views.Intel()
-	intel_with_navigation := layouts.WithNavigation(intel)
-	if err := save_document(output_dir+"/intel.html", intel_with_navigation); err != nil {
-		log.Fatalf("failed to save document: %v", err)
-	}
-
-	about := views.About()
-	about_with_navigation := layouts.WithNavigation(about)
-	if err := save_document(output_dir+"/about.html", about_with_navigation); err != nil {
-		log.Fatalf("failed to save document: %v", err)
-	}
-
-	help := views.Help()
-	help_with_navigation := layouts.WithNavigation(help)
-	if err := save_document(output_dir+"/help.html", help_with_navigation); err != nil {
-		log.Fatalf("failed to save document: %v", err)
-	}
-
-	contact := views.Contact()
-	contact_with_navigation := layouts.WithNavigation(contact)
-	if err := save_document(output_dir+"/contact.html", contact_with_navigation); err != nil {
-		log.Fatalf("failed to save document: %v", err)
-	}
-
 }
