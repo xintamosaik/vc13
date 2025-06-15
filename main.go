9ef7c0795040393c0d2ff06cba6f8f4310513075
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-06-15 13:45:36 +0200

    refactor: update navigation links and add new views for Drafts, Signals, Intel, Help, and Contact

--- main.go
+++ main.go
@@ -40,17 +40,46 @@ func main() {
 		log.Fatalf("failed to save document: %v", err)
 	}
 
-	example := views.Example()
+	// example := views.Example()
 
-	example_with_navigation := components.WithNavigation(example)
+	// example_with_navigation := components.WithNavigation(example)
 
-	if err := save_document(output_dir+"/example.html", example_with_navigation); err != nil {
+	// if err := save_document(output_dir+"/example.html", example_with_navigation); err != nil {
+	// 	log.Fatalf("failed to save document: %v", err)
+	// }
+	drafts := views.Drafts()
+	drafts_with_navigation := components.WithNavigation(drafts)
+	if err := save_document(output_dir+"/drafts.html", drafts_with_navigation); err != nil {
+		log.Fatalf("failed to save document: %v", err)
+	}
+
+	signals := views.Signals()
+	signals_with_navigation := components.WithNavigation(signals)
+	if err := save_document(output_dir+"/signals.html", signals_with_navigation); err != nil {
+		log.Fatalf("failed to save document: %v", err)
+	}
+
+	intel := views.Intel()
+	intel_with_navigation := components.WithNavigation(intel)
+	if err := save_document(output_dir+"/intel.html", intel_with_navigation); err != nil {
 		log.Fatalf("failed to save document: %v", err)
 	}
 
 	about := views.About()
 	about_with_navigation := components.WithNavigation(about)
 	if err := save_document(output_dir+"/about.html", about_with_navigation); err != nil {
+		log.Fatalf("failed to save document: %v", err)
+	}
+
+	help := views.Help()
+	help_with_navigation := components.WithNavigation(help)
+	if err := save_document(output_dir+"/help.html", help_with_navigation); err != nil {
+		log.Fatalf("failed to save document: %v", err)
+	}
+
+	contact := views.Contact()
+	contact_with_navigation := components.WithNavigation(contact)
+	if err := save_document(output_dir+"/contact.html", contact_with_navigation); err != nil {
 		log.Fatalf("failed to save document: %v", err)
 	}
 
